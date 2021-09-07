524dd3ab5ee0c45f54dc56253641f90b6026ad43
Asmaa Maher <81307909+Asmaa601@users.noreply.github.com>
2021-09-08 01:44:59 +0200

    Update tx.v

--- tx.v
+++ tx.v
@@ -1,45 +1,50 @@
 // transmitter module for UART
 
 module tx_uart(
-input clk, rst, enable ,
-input [7:0] tx_data,
-output reg tx_bit 
+input clk, rst, enable ,clk_handle,
+input [7:0] tx_data_i,
+output  tx_bit_o,
+output  Ready 
 );
 
-reg state, next_state;
-parameter IDLE = 1'b0, SEND = 1'b1; 
-integer count=0 ,start_bit = 0;   
 
-// sequential logic
-always @(posedge clk or negedge rst)
+reg tx_bit;
+reg [1:0]state, next_state;
+parameter IDLE = 2'b00, START = 2'b01, DATA =2'b10, STOP = 2'b11; 
+reg [2:0] index = 3'b0;
+reg [7:0] tx_data;
+
+assign tx_bit_o = tx_bit;
+assign Ready = (state == IDLE);
+
+initial begin tx_bit <= 1'b1; end
+always @(posedge clk, negedge rst)
 begin
-    if (!rst) state <= 1'b0;
-    else state <= next_state;
+if (!rst) state <= IDLE;
+else state <= next_state; 
 end
 
-// set states of the UART transmitter
-always @(*)
+always @(posedge clk)
 begin
-    case (state)
-    IDLE: begin 
-        if (enable) next_state = SEND ;
-        else next_state = IDLE;
-         end
-    SEND: begin if (count == 8) next_state = IDLE; 
-         else next_state = SEND;
-          end
-    endcase
-end
-
-// define each state job
-always @(*)
-begin
-    case (state)
-    IDLE: begin tx_bit = 1'b0; end
-    SEND: begin 
-        if (count && start_bit ==0 ) begin tx_bit =1'b1; start_bit = 1;end 
-        else begin tx_bit = tx_data[count]; count = count + 1; end
-        end
-    endcase
-end
+case (state)
+IDLE: begin 
+     if (enable) begin next_state <= START; tx_data <= tx_data_i; index <= 3'b0;end 
+      end
+START: begin
+     if (clk_handle) begin tx_bit <= 1'b0;  next_state <= DATA; end 
+     end
+DATA: begin 
+    if (clk_handle) begin tx_bit <= tx_data[index]; 
+                        if (index == 3'h7) begin   next_state <= STOP; end 
+                        else  index <= index + 3'b1; 
+                    end 
+     end
+STOP: begin 
+    if (clk_handle)begin tx_bit <= 1'b1; next_state <= IDLE; end 
+    end
+default: begin 
+    tx_bit <= 1'b1; next_state <= IDLE; 
+    end
+endcase 
+end 
 endmodule
